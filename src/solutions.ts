5454071ea12c0d3c5051a9c6959f0c1848a944d2
alminsfd <alaminhossaintanvir42@gmail.com>
2026-05-08 08:27:42 +0600

    solve the problem 6

--- src/solutions.ts
+++ src/solutions.ts
@@ -48,3 +48,31 @@ const toggleReadStatus = (books: Book) => {
      }
 
 }
+
+// problem-06
+
+class Person {
+     name: string
+     age: number
+     constructor(name: string, age: number) {
+          this.name = name,
+               this.age = age
+     }
+
+}
+
+class Student extends Person {
+     grade: string
+
+     constructor(name: string, age: number, grade: string) {
+          super(name, age)
+          this.grade = grade
+     }
+
+     getDetails() {
+          return ` "Name:${this.name} , Age: ${this.age} , Grade: ${this.grade}"`
+     }
+}
+
+
+// problem-07
